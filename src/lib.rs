6678a4fc81959c306eea70f564396eda52f543f3
Oleksii Skrypka <oleksii.skrypka@globallogic.com>
2018-05-25 13:59:19 +0300

    Print available memory info

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,18 @@ extern crate spin;
 
 #[no_mangle]
 pub extern fn rust_main(multiboot_information_address: usize) {
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag()
+        .expect("Memory map tag required");
 
     vga_buffer::clear_screen();
 
-    println!("Hello World{}", "!");
-    println!("{}", { println!("inner"); "outer" });
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("  start: 0x{:x}, length: 0x{:x}",
+            area.base_addr, area.length);
+    }
+
     loop{}
 }
 
