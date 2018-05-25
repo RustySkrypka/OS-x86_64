c1dd7d9bc32b8c975de098b30fc106aeb192d655
Oleksii Skrypka <oleksii.skrypka@globallogic.com>
2018-05-25 13:22:15 +0300

    Add VGA buffer Interface though println!

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
+#![feature(const_unique_new)]
+#![feature(unique)]
 #![feature(lang_items)]
 #![no_std] 
+#![feature(const_fn)]
+#![feature(ptr_internals)]
+
+#[macro_use]
+mod vga_buffer;
 
 extern crate rlibc;
+extern crate volatile;
+extern crate spin;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let x = ["Hello", "World", "!"];
-    let y = x;
+//    use core::fmt::Write;
+//    vga_buffer::WRITER.lock().write_str("Hello again");
+//    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
+
+    vga_buffer::clear_screen();
+
+    println!("Hello World{}", "!");
+    println!("{}", { println!("inner"); "outer" });
+    loop{}
 }
 
 #[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personal() {}
